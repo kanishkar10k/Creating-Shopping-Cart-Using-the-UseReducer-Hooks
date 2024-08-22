9ae967945b70dc1b6743aa6c47866ebea91384c7
Kanishkar <720721110014@hicet.ac.in>
2024-08-22 17:50:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
-
+import cart from './images/cart.png';
+import muskmelon from './images/muskmelon.png';
+import apple from './images/apple.png';
+import orange from './images/orange.png';
+import pineapple from './images/pineapple.png';
+import watermelon  from './images/watermelon.png';
+import banana from './images/banana.png';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <div class="logo">
+            <h1>Fruit Basket</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Product</a></li>
+                <li><a href="#"><img src={cart} id='cart'></img> </a></li>
+            </ul>
+        </nav>
       </header>
+      <main>
+      <h2>Featured Products</h2><br></br>
+        <section class="products">
+              <div class="product-card">
+                <img src={apple} alt="Apple"></img>
+                <h3>Apple</h3>
+                <p>Rs.180 Per KG</p>
+                <button>Add to Cart</button>
+              </div>
+              <div class="product-card">
+                <img src={banana} alt="Banana"></img>
+                <h3>Banana</h3>
+                <p>Rs.60 Per KG</p>
+                <button>Add to Cart</button>
+              </div>
+              <div class="product-card">
+                <img src={watermelon} alt="Watermelon"></img>
+                <h3>Watermelon</h3>
+                <p>Rs.120 Per KG</p>
+                <button>Add to Cart</button>
+              </div>
+              <div class="product-card">
+                <img src={pineapple} alt="pineapple"></img>
+                <h3>Pineapple</h3>
+                <p>Rs.100 Per KG</p>
+                <button>Add to Cart</button>
+              </div>
+              <div class="product-card">
+                <img src={orange} alt="Orange"></img>
+                <h3>Orange</h3>
+                <p>Rs.160 Per KG</p>
+                <button>Add to Cart</button>
+              </div>
+              <div class="product-card">
+                <img src={muskmelon} alt="Muskmelon"></img>
+                <h3>Muskmelon</h3>
+                <p>Rs.180 Per KG</p>
+                <button>Add to Cart</button>
+              </div>
+          </section>
+        </main>
+        <footer>
+        <p>&copy; 2024 Grocery Shop. All rights reserved.</p>
+    </footer>
     </div>
   );
 }
